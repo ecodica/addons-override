8e828cbe17086e4385238f18b7e4c09665d66e36
danijel <danijel.bosnar@ecodica.eu>
2025-03-17 16:41:48 +0100

    [2442;MAZARS;IMP,FIX] hr_employee_birth_name: reverse order of modifying hr employee public and private models
    *since it crashes if public is ahead of private since public uses private as source

--- hr_employee_birth_name/models/__init__.py
+++ hr_employee_birth_name/models/__init__.py
@@ -3,5 +3,5 @@
 # Copyright (C) 2021 Serpent Consulting Services
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import hr_employee_public
 from . import hr_employee
+from . import hr_employee_public
