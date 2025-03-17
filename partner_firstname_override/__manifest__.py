bddeddb370036ed7dc5f4c554ce83a27cc9a4ef8
Luka <lvrabec.101@gmail.com>
2025-03-17 12:08:10 +0100

    [ADD] partner_firstname_override

---
+++ partner_firstname_override/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "OCA partner firstname override",
+    "summary": "Overrides the default_get name computation in res.partner",
+    "category": "Accounting",
+    "version": "17.0.1.0.0",
+    "author": "info3",
+    "website": "https://info3.hr/hr/",
+    "license": "OPL-1",
+    "depends": [
+        "partner_firstname",
+    ],
+    "data": [],
+    'auto_install': ['partner_firstname'],
+    "application": False,
+    "installable": True,
+}
---
+++ partner_firstname_override/readme/USAGE.rst
@@ -0,0 +1,1 @@
+No useage changes.
