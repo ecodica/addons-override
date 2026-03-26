ef08ca5a83ed408142f2e8d52a67e84f3ab07cc2
Mario Semialjac <mario.semialjac@ecodica.eu>
2026-03-26 22:49:02 +0100

    [Ecodica;ADD] auth_admin_passkey_extended: add module from custom_addons

---
+++ auth_admin_passkey_extended/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Ecodica d.o.o.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Auth Admin Passkey - TOTP Bypass",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "summary": "Skip TOTP/2FA verification when admin passkey is used",
+    "author": "Ecodica d.o.o.",
+    "website": "https://ecodica.eu",
+    "license": "AGPL-3",
+    "depends": ["auth_admin_passkey"],
+    "installable": True,
+}
